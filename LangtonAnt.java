e4ecc6bbbe9a5d64e997c08f59caa81aaec3a248
chrismilson <chris.w.milson@gmail.com>
2016-06-20 18:21:19 +1200

    Initial commit

---
+++ LangtonAnt.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import javax.swing.*;
+
+public class LangtonAnt {
+  public static void main (String[] args) {
+    System.out.println("Hello World!");
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LangtonAnt
 A Java implementation of the Langton's Ant cellular automaton
+
+A project for the learnings of yours truly.
